5fcde1a4ea2e749bbcfea00ec191a5e3b1a34281
Dhiaslim <dhiaslim4@gmail.com>
2022-01-13 16:25:29 +0000

    update1

--- Assets/ball.cs
+++ Assets/ball.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class ball : MonoBehaviour
 {
     public float speed = 30;
-
-    Vector2 speed;
     // Update is called once per frame
     void Start()
     {
         Vector2 speed = new Vector2(1,1);
         GetComponent<Rigidbody2D>().velocity = speed;
         // Initial Velocity
-        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        GetComponent<Rigidbody2D>().velocity = Vector2.right*speed;
     }
 }
